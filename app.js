0df38f12c06b8b591ae284858d21f328c5bf6e05
Anoop Raj <anoopraj46@gmail.com>
2021-06-19 12:40:11 +0530

    Update App.js

--- app.js
+++ app.js
@@ -14,6 +14,31 @@
 
  app.use(express.static("public"));
 
+ mongoose.connect("mongodb://localhost:27017/Ass1db",{useNewUrlParser: true, useUnifiedTopology: true});
+
+ const deviceSchema = {
+   device_id : Number,
+   device_name: String
+ };
+
+ const locationSchema = {
+   device_id : Number,
+   locations: String
+ };
+
+const Device = mongoose.model("Device", deviceSchema);
+const Location = mongoose.model("Location", locationSchema);
+
+app.get("/devices", function(req, res){
+  Device.find(function(err, foundDevices){
+    if(!err){
+    console.log(foundDevices);
+  }
+  else{
+    console.log(err);
+  }
+  })
+})
 
  app.listen(3000, function(){
    console.log("Server started on port 3000");
